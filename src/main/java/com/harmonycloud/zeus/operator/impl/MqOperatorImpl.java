f5883f42ecfabf84370c8ccaa935505a95eb336d
xutianhong <736950061@qq.com>
2021-08-26 14:18:58 +0800

    fix - rocketMQ acl认证允许全局白名单为空

--- src/main/java/com/harmonycloud/zeus/operator/impl/MqOperatorImpl.java
+++ src/main/java/com/harmonycloud/zeus/operator/impl/MqOperatorImpl.java
@@ -274,13 +274,15 @@ public class MqOperatorImpl extends AbstractMqOperator implements MqOperator {
                 RocketMQParam param = new RocketMQParam();
                 RocketMQACL acl = new RocketMQACL();
 
-                JSONArray globalWhiteRemoteAddresses = jsonAcl.getJSONArray("globalWhiteRemoteAddresses");
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < globalWhiteRemoteAddresses.size(); ++i) {
-                    builder.append(globalWhiteRemoteAddresses.get(i)).append(";");
-                }
-                acl.setGlobalWhiteRemoteAddresses(builder.toString());
                 acl.setEnable(true);
+                if (jsonAcl.containsKey("globalWhiteRemoteAddresses")) {
+                    JSONArray globalWhiteRemoteAddresses = jsonAcl.getJSONArray("globalWhiteRemoteAddresses");
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < globalWhiteRemoteAddresses.size(); ++i) {
+                        builder.append(globalWhiteRemoteAddresses.get(i)).append(";");
+                    }
+                    acl.setGlobalWhiteRemoteAddresses(builder.toString());
+                }
 
                 List<RocketMQAccount> accountList = new ArrayList<>();
                 JSONArray accounts = jsonAcl.getJSONArray("accounts");
